27d18d450f1c49f46ec65465bb9e12ac3146cccd
Corey Cheung <coreycheung@outlook.com>
2023-12-28 20:50:27 +1000

    main function for upsert, passed linter

--- src/update_nba_games.py
+++ src/update_nba_games.py
@@ -1,29 +1,45 @@
 #!/usr/bin/env python
-
+"""
+to do blah blah
+"""
 # - get max game_date where status is 'Final'
 # - query the API with that date and the preceeding dates
 # - format the data into insert values
 # - write an upsert query to add the rows
+# get american timezone(?)
+
+from datetime import datetime, timedelta
 
 import requests
-from datetime import datetime, timedelta
-from nba_pg_ingestion_utils import query_postgres, get_row_to_insert
+
 from get_nba_games import format_games_data
+from nba_pg_ingestion_utils import (
+    generate_db_objects,
+    get_row_to_insert,
+    query_postgres,
+)
+
 # import pytz
 
-def get_start_and_end_dates() -> tuple[str, str]:
+
+def get_start_and_end_dates(lookback) -> tuple[str, str]:
     """
     blah
     """
+    print(f"lookback days: {lookback}")
     query = """ SELECT MAX(game_date) FROM nba_basketball.game WHERE status = 'Final';
     """
     max_game_date = query_postgres(query)[0]
-    start_date = (max_game_date - timedelta(days=2)).strftime("%Y-%m-%d")
+    start_date = (max_game_date - timedelta(days=lookback)).strftime("%Y-%m-%d")
     end_date = datetime.now().date().strftime("%Y-%m-%d")
 
     return start_date, end_date
 
+
 def get_games_to_update(url: str, start_date: str, end_date: str) -> str:
+    """
+    blah
+    """
     current_page = 1
     total_pages = 1
     to_upsert = ""
@@ -43,7 +59,7 @@ def get_games_to_update(url: str, start_date: str, end_date: str) -> str:
 
             for row in data:
                 formatted = format_games_data(row)
-                row_to_insert = "("+get_row_to_insert(formatted)+")"
+                row_to_insert = "(" + get_row_to_insert(formatted) + ")"
                 to_upsert += f"\n{row_to_insert},"
 
             current_page = meta["current_page"] + 1
@@ -55,42 +71,50 @@ def get_games_to_update(url: str, start_date: str, end_date: str) -> str:
     return to_upsert
 
 
-start_date, end_date = get_start_and_end_dates()
-print(f"Updating the games table with games between {start_date} and {end_date}.")
+def main(lookback):
+    """
+    blah
+    """
+    start_date, end_date = get_start_and_end_dates(lookback=lookback)
+    print(f"Updating the games table with games between {start_date} and {end_date}.")
 
-to_upsert = get_games_to_update(
-    url="https://www.balldontlie.io/api/v1/games",
-    start_date=start_date,
-    end_date=end_date
+    to_upsert = get_games_to_update(
+        url="https://www.balldontlie.io/api/v1/games",
+        start_date=start_date,
+        end_date=end_date,
     )
-to_upsert = to_upsert[:-1] # get rid of last comma
+    to_upsert = to_upsert[:-1]  # get rid of last comma
 
-upsert_query = """
-INSERT INTO nba_basketball.game_test
-(
-    game_id,
-    game_date,
-    home_team_id,
-    home_team_score,
-    visitor_team_id,
-    visitor_team_score,
-    season,
-    post_season,
-    status
-    )
-VALUES"""
-upsert_query += to_upsert
-upsert_query += """
-ON CONFLICT (game_id) DO UPDATE SET
-    game_id = EXCLUDED.game_id,
-    game_date = EXCLUDED.game_date,
-    home_team_id = EXCLUDED.home_team_id,
-    home_team_score = EXCLUDED.home_team_score,
-    visitor_team_id = EXCLUDED.visitor_team_id,
-    visitor_team_score = EXCLUDED.visitor_team_score,
-    season = EXCLUDED.season,
-    post_season = EXCLUDED.post_season,
-    status = EXCLUDED.status
-;
-"""
-print(upsert_query)
+    upsert_query = """
+    INSERT INTO nba_basketball.game
+    (
+        game_id,
+        game_date,
+        home_team_id,
+        home_team_score,
+        visitor_team_id,
+        visitor_team_score,
+        season,
+        post_season,
+        status
+        )
+    VALUES"""
+    upsert_query += to_upsert
+    upsert_query += """
+    ON CONFLICT (game_id) DO UPDATE SET
+        game_id = EXCLUDED.game_id,
+        game_date = EXCLUDED.game_date,
+        home_team_id = EXCLUDED.home_team_id,
+        home_team_score = EXCLUDED.home_team_score,
+        visitor_team_id = EXCLUDED.visitor_team_id,
+        visitor_team_score = EXCLUDED.visitor_team_score,
+        season = EXCLUDED.season,
+        post_season = EXCLUDED.post_season,
+        status = EXCLUDED.status
+    ;
+    """
+    generate_db_objects(upsert_query)
+
+
+if __name__ == "__main__":
+    main(lookback=2)
